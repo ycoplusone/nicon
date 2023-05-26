e7672a7c173ff00de49280229ea7f2a887f0be17
ycoplusone <58333620+ycoplusone@users.noreply.github.com>
2023-05-26 17:47:55 +0900

    Add files via upload

--- nicon/app.py
+++ nicon/app.py
@@ -181,7 +181,7 @@ def fn_test():
     print('aaa')
 
 if __name__ == "__main__":    
-    schedule.every(10).seconds.do( getNicon )
+    schedule.every(30).seconds.do( getNicon )
     schedule.every(1).days.do( fn_history )
     while True:
         schedule.run_pending()
