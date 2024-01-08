6965bff2db537a4197ec748535c331b62ffd407c
ycoplusone <58333620+ycoplusone@users.noreply.github.com>
2024-01-08 14:29:57 +0900

    Add files via upload
    상품중복제거

--- nicon/auto.py
+++ nicon/auto.py
@@ -10,16 +10,20 @@ def main():
     try:
         while(True):
             _ret = False
+            _cnt = 0
             for pid in psutil.pids():
                 p = psutil.Process(pid)
-                if 'chromedriver.exe' == p.name():
-                    _ret = True
-            if _ret:
-                print('실행중')
+                #if 'chromedriver.exe' == p.name():
+                #    _ret = True
+                if 'python' in p.name():
+                    print(p)
+                    _cnt += 1
+            if _cnt >=2 :
+                print('실행중','*'*100)
             else:
                 print('정지중')
                 os.system("python C:\\Users\\DLIVE\\eclipse-workspace\\nicon\\autonicon2.py")
-            time.sleep(60)
+            time.sleep(20)
             
     except Exception as e:
         print('auto == ' , e)
