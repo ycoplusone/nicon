0beb2c9b8168c8f63abfd62df629be3cb2aeea5b
ycoplusone <58333620+ycoplusone@users.noreply.github.com>
2024-03-11 09:37:26 +0900

    Add files via upload

--- nicon/app_ex.py
+++ nicon/app_ex.py
@@ -91,123 +91,127 @@ def visa():
         
 def cal_rating( _param ):
     '''환율 계산'''   
-    _min = float( _param['min'] )
-    _max = float( _param['max'] )
-    _step = float( _param['step'] )
-    _correction_amt = float( _param['correction_amt'] )
-    # db 객체
-    dd = dbcon.DbConn()    
+    try:
+        _min = float( _param['min'] )
+        _max = float( _param['max'] )
+        _step = float( _param['step'] )
+        _correction_amt = float( _param['correction_amt'] )
+        # db 객체
+        dd = dbcon.DbConn()    
 
-    # 환율정보
-    _data = dd.get_ex_info( {'CUR_ID':_param['ex_id']} )    
-    
-    if len( _data ) != 0:   
-        _ex_info = _data[0]  
-    
-        _rate           = float( _ex_info['RATE'] )             #환율
-        _bank_rate      = float( _ex_info['BANK_FIX_RATE'] )    #고시환율1차    
-        _glo_brand_fee  = 0.011                     #국제브랜드수수료
-        _for_sv_fee     = 0.0018                    #해외서비스수수료
+        # 환율정보
+        _data = dd.get_ex_info( {'CUR_ID':_param['ex_id']} )    
+        
+        if len( _data ) != 0:   
+            _ex_info = _data[0]  
+        
+            _rate           = float( _ex_info['RATE'] )             #환율
+            _bank_rate      = float( _ex_info['BANK_FIX_RATE'] )    #고시환율1차    
+            _glo_brand_fee  = 0.011                     #국제브랜드수수료
+            _for_sv_fee     = 0.0018                    #해외서비스수수료
 
-        def rounddown(num):
-            #내림
-            return math.floor( num * 100) / 100
-        
-        _lists = []
-        for i in list(np.arange( _min , _max , _step )):
-            _amt = round(i,2) #수량
-            _aa1 = math.trunc( rounddown( round( _amt * _rate,2 ) * (1+_glo_brand_fee) ) * _bank_rate  )
-            _aa2 = math.trunc( _aa1 * _for_sv_fee )   
-            _aa3 = _aa1 + _aa2
-            _aa4 = round(( _aa3 % 1000) * 2 / _aa3 ,4) if _aa3 != 0  else 0 
-            _tmp = { 'BASE_DT': _ex_info['BASE_DT'],'CUR_ID': _ex_info['CUR_ID'] , 'QTY':_amt , 'AMT':_aa3 , 'ACC': _aa4}
-            _lists.append( _tmp )
+            def rounddown(num):
+                #내림
+                return math.floor( num * 100) / 100
+            
+            _lists = []
+            for i in list(np.arange( _min , _max , _step )):
+                _amt = round(i,2) #수량
+                _aa1 = math.trunc( rounddown( round( _amt * _rate,2 ) * (1+_glo_brand_fee) ) * _bank_rate  )
+                _aa2 = math.trunc( _aa1 * _for_sv_fee )   
+                _aa3 = _aa1 + _aa2
+                _aa4 = round(( _aa3 % 1000) * 2 / _aa3 ,4) if _aa3 != 0  else 0 
+                _tmp = { 'BASE_DT': _ex_info['BASE_DT'],'CUR_ID': _ex_info['CUR_ID'] , 'QTY':_amt , 'AMT':_aa3 , 'ACC': _aa4}
+                _lists.append( _tmp )
 
-        _amt_05 = {}
-        _amt_06 = {}
-        _amt_07 = {}
-        _amt_08 = {}
-        _amt_09 = {}
-        _amt_10 = {}
-        _amt_11 = {}
-        _amt_12 = {}
-        _amt_13 = {}
-        _amt_14 = {}
-        _amt_15 = {}
-        _amt_16 = {}
-        _amt_17 = {}
-        _amt_tot = []
-        
-        for _list in _lists:
-            if _list['AMT'] <= 5999:
-                _amt_05 = _list
-                _amt_05['kind'] = '5999'
-            elif _list['AMT'] <= 6999:
-                _amt_06 = _list
-                _amt_06['kind'] = '6999'
-            elif _list['AMT'] <= 7999:
-                _amt_07 = _list
-                _amt_07['kind'] = '7999'
-            elif _list['AMT'] <= 8999:
-                _amt_08 = _list
-                _amt_08['kind'] = '8999'
-            elif _list['AMT'] <= 9999:
-                _amt_09 = _list
-                _amt_09['kind'] = '9999'
-            elif _list['AMT'] <= 10999:
-                _amt_10 = _list
-                _amt_10['kind'] = '10999'
-            elif _list['AMT'] <= 11999:
-                _amt_11 = _list
-                _amt_11['kind'] = '11999'
-            elif _list['AMT'] <= 12999:
-                _amt_12 = _list
-                _amt_12['kind'] = '12999'
-            elif _list['AMT'] <= 13999:
-                _amt_13 = _list
-                _amt_13['kind'] = '13999'
-            elif _list['AMT'] <= 14999:
-                _amt_14 = _list
-                _amt_14['kind'] = '14999'
-            elif _list['AMT'] <= 15999:
-                _amt_15 = _list
-                _amt_15['kind'] = '15999'
-            elif _list['AMT'] <= 16999:
-                _amt_16 = _list
-                _amt_16['kind'] = '16999'
-            elif _list['AMT'] <= 17999:
-                _amt_17 = _list
-                _amt_17['kind'] = '17999'
-
-        _amt_tot.append( _amt_05 )
-        _amt_tot.append( _amt_06 )
-        _amt_tot.append( _amt_07 )
-        _amt_tot.append( _amt_08 )
-        _amt_tot.append( _amt_09 )
-        _amt_tot.append( _amt_10 )
-        _amt_tot.append( _amt_11 )
-        _amt_tot.append( _amt_12 )
-        _amt_tot.append( _amt_13 )
-        _amt_tot.append( _amt_14 )
-        _amt_tot.append( _amt_15 )
-        _amt_tot.append( _amt_16 )
-        _amt_tot.append( _amt_17 )
-        
-        
-        # 보정 데이터 찾기
-        _amt_tot2 = []
-        for i in _amt_tot:
-            _chk_amt = round(i['QTY'] + _correction_amt ,2)
+            _amt_05 = {}
+            _amt_06 = {}
+            _amt_07 = {}
+            _amt_08 = {}
+            _amt_09 = {}
+            _amt_10 = {}
+            _amt_11 = {}
+            _amt_12 = {}
+            _amt_13 = {}
+            _amt_14 = {}
+            _amt_15 = {}
+            _amt_16 = {}
+            _amt_17 = {}
+            _amt_tot = []
+            
             for _list in _lists:
-                if _list['QTY'] == _chk_amt:
-                    _tmp = _list
-                    _tmp['kind'] = i['kind']
-                    _amt_tot2.append(  _tmp )
-        
-        
-        # 기록 하는 부분
-        for i in _amt_tot2:
-            dd.upsert_ex_info2( i )
+                if _list['AMT'] <= 5999:
+                    _amt_05 = _list
+                    _amt_05['kind'] = '5999'
+                elif _list['AMT'] <= 6999:
+                    _amt_06 = _list
+                    _amt_06['kind'] = '6999'
+                elif _list['AMT'] <= 7999:
+                    _amt_07 = _list
+                    _amt_07['kind'] = '7999'
+                elif _list['AMT'] <= 8999:
+                    _amt_08 = _list
+                    _amt_08['kind'] = '8999'
+                elif _list['AMT'] <= 9999:
+                    _amt_09 = _list
+                    _amt_09['kind'] = '9999'
+                elif _list['AMT'] <= 10999:
+                    _amt_10 = _list
+                    _amt_10['kind'] = '10999'
+                elif _list['AMT'] <= 11999:
+                    _amt_11 = _list
+                    _amt_11['kind'] = '11999'
+                elif _list['AMT'] <= 12999:
+                    _amt_12 = _list
+                    _amt_12['kind'] = '12999'
+                elif _list['AMT'] <= 13999:
+                    _amt_13 = _list
+                    _amt_13['kind'] = '13999'
+                elif _list['AMT'] <= 14999:
+                    _amt_14 = _list
+                    _amt_14['kind'] = '14999'
+                elif _list['AMT'] <= 15999:
+                    _amt_15 = _list
+                    _amt_15['kind'] = '15999'
+                elif _list['AMT'] <= 16999:
+                    _amt_16 = _list
+                    _amt_16['kind'] = '16999'
+                elif _list['AMT'] <= 17999:
+                    _amt_17 = _list
+                    _amt_17['kind'] = '17999'
+
+            _amt_tot.append( _amt_05 )
+            _amt_tot.append( _amt_06 )
+            _amt_tot.append( _amt_07 )
+            _amt_tot.append( _amt_08 )
+            _amt_tot.append( _amt_09 )
+            _amt_tot.append( _amt_10 )
+            _amt_tot.append( _amt_11 )
+            _amt_tot.append( _amt_12 )
+            _amt_tot.append( _amt_13 )
+            _amt_tot.append( _amt_14 )
+            _amt_tot.append( _amt_15 )
+            _amt_tot.append( _amt_16 )
+            _amt_tot.append( _amt_17 )
+            
+            
+            # 보정 데이터 찾기
+            _amt_tot2 = []
+            for i in _amt_tot:
+                #print( i  , _correction_amt)
+                _chk_amt = round(i['QTY'] + _correction_amt ,2)
+                for _list in _lists:
+                    if _list['QTY'] == _chk_amt:
+                        _tmp = _list
+                        _tmp['kind'] = i['kind']
+                        _amt_tot2.append(  _tmp )
+            
+            
+            # 기록 하는 부분
+            for i in _amt_tot2:
+                dd.upsert_ex_info2( i )
+    except Exception as e:
+        print( 'error : ' , _param , ' / ',e )
     
     
 def get_ex_data():
