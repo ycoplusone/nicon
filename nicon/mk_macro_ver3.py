baf0ebc8f04812d1d3a657de126295c5ce2dd6cf
ycoplusone <58333620+ycoplusone@users.noreply.github.com>
2024-12-12 09:42:46 +0900

    Add files via upload

--- nicon/mk_macro_ver3.py
+++ nicon/mk_macro_ver3.py
@@ -295,7 +295,7 @@ class Work(QThread):
                                     _minute      = int((totalTime//60)%60)   # 초를 분으로 환산하여 60으로 나눈 나머지
                                     _hour        = int(totalTime//60//60)    # 초를 분으로 환산하고, 그 분을 시간으로 환산한 몫
 
-                                    msg         = f'1회 소요시간 : {round(useTime,2)}초 \n 전체횟수 : {totalCnt}건 \n 전체소요시간 : {_hour}:{_minute}:{_second} \n 예상시간 : {predictTime}\n 작업명 : {self.__file_nm}'
+                                    msg         = f'1회 소요시간 [{round(useTime,2)}초] \n 전체횟수 [{totalCnt}건] \n 전체소요시간 [{_hour}:{_minute}:{_second}] \n 예상시간 [{predictTime}]\n 작업명 [{self.__file_nm}]'
                                     print('소요시간 산출 - 시작','*'*20)
                                     print(beTime , asTime , useTime)
                                     print(msg)
@@ -325,7 +325,7 @@ class Work(QThread):
             self.__power = False
         
         if self.__power == True:
-            w2ji.send_telegram_message( f'Version {self.__version}\n 예상시간 : {predictTime} \n 작업명 : {self.__file_nm}\n 완료되었습니다.' )
+            w2ji.send_telegram_message( f'Version {self.__version}\n 예상시간 [{predictTime}] \n 작업명 [{self.__file_nm}]\n 완료되었습니다.' )
             pyautogui.alert('완료 되었습니다.')
             
         else :
