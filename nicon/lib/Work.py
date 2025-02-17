9a1bcd0221ecad4754f740fa95228bb00665d851
ycoplusone <58333620+ycoplusone@users.noreply.github.com>
2025-02-17 13:39:27 +0900

    1.0.3
     ctrl+a,ctrl+c,ctrl+v 추가 , '자방[숫자]' 기능 추가 작업

--- nicon/lib/Work.py
+++ nicon/lib/Work.py
@@ -11,8 +11,12 @@ import pyperclip
 import time
 import random
 import os
+import base64
+import openai
+from dotenv import load_dotenv      # pip install python-dotenv , pip install dotenv 
 
 ''' 자동화 클래스
+1.0.3 ctrl+a,ctrl+c,ctrl+v 추가 , '자방[숫자]' 기능 추가 작업
 1.0.2 붙여넣기 0.5초 고정 대기 시간 부여
 1.0.1 자동화 클랙스 분리
 '''
@@ -45,6 +49,7 @@ class Work(QThread):
     __power = False
 
     def __init__(self ):
+        load_dotenv()   #환경변수 로딩    
         super().__init__()
         self.__power = True     # run 매소드 루프 플래그
 
@@ -122,7 +127,14 @@ class Work(QThread):
     
     def fnkey(self , str , cnt):
         '''키 입력'''
-        pyautogui.press( str , presses = cnt , interval=0.2)        
+        if str == 'ctrl+a': # 전체선택
+            pyautogui.hotkey('ctrl', 'a' , interval=0.2)            
+        elif str == 'ctrl+c': # 복사
+            pyautogui.hotkey('ctrl', 'c' , interval=0.2)            
+        elif str == 'ctrl+v': # 붙여넣기
+            pyautogui.hotkey('ctrl', 'v' , interval=0.2)                        
+        else : # 이외 전체 키 처리.
+            pyautogui.press( str , presses = cnt , interval=0.2)        
 
     def fnArrayGet(self , arr , pos):
         '''배열 검색후 리턴'''
@@ -246,6 +258,67 @@ class Work(QThread):
             pyautogui.moveTo( d1 )          # 마우스 이동     
             pyautogui.dragTo(   d2.x , d2.y   , duration= 0.3)
             time.sleep( float(xy_wait) )    #대기   
+        
+        elif( step_name == '자방[숫자]') and ( self.__power == True ) :
+            '''자동등록방지[숫자] 인식 구간.'''
+            # 캡쳐후 해당 부분 챗GPT에 인식후 복사하는 방식.
+            # 캡쳐후 인식한 숫자를 파일에 같이 넣어 이후 신뢰도를 확인한다.
+            r11 = rand.get(11)
+            r12 = rand.get(12)
+            capture_width   = r12.x - r11.x
+            capture_height  = r12.y - r11.y
+
+            base_dttm   = datetime.now(timezone('Asia/Seoul')).strftime('%Y%m%d_%H%M%S')
+            base_dt     = datetime.now(timezone('Asia/Seoul')).strftime('%Y%m%d')    
+            try:
+                os.mkdir('c:\\ncnc_class\\recapcha')
+            except Exception as e:
+                '''폴더 생성 있으면 넘어간다.'''  
+            file_nm     = base_dttm+'_'+str(_j[0])
+            file_name   = f"c:\\ncnc_class\\recapcha\\{file_nm}.png"
+            pyautogui.screenshot( file_name , region=(r11.x , r11.y , capture_width, capture_height))    
+            
+            def encode_image(image_path):
+                """이미지를 Base64로 인코딩하는 함수"""
+                with open(image_path, "rb") as image_file:
+                    return base64.b64encode(image_file.read()).decode("utf-8")
+            
+            def recognize_numbers(image_path):
+                """
+                이미지에서 숫자를 인식하는 함수 (GPT-4-Vision 사용)
+                """
+                openai.api_key = os.environ.get('open_api_key')
+
+                base64_image = encode_image(image_path)  # 이미지 Base64 변환
+
+                response = openai.ChatCompletion.create(
+                    model="gpt-4o",  # GPT-4-Vision 모델 사용
+                    messages=[
+                        {"role": "system", "content": "당신은 이미지에서 숫자를 정확하게 인식하는 AI입니다."},
+                        {"role": "user", "content": [
+                            {"type": "text", "text": "숫자만 읽어줘"},
+                            {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"}}
+                        ]}
+                    ],
+                    max_tokens=100
+                )
+                # 이 이미지에 있는 숫자를 모두 읽어주세요.
+                return response["choices"][0]["message"]["content"]
+            
+            capcha_number = recognize_numbers(file_name) 
+            capcha_number = re.findall(r"\d+", capcha_number)[0] # 숫자만 산출하는 정규식
+            new_file_name   = f"c:\\ncnc_class\\recapcha\\{file_nm}_{capcha_number}.png" # 새로 변경할 파일 이름
+            # 파일 이름 변환.
+            os.rename(file_name ,new_file_name ) 
+            
+            self.fnclick( xy , self.__waitTime ) #클릭
+                        
+            ## 붙여넣기
+            self.fnpaste( capcha_number ) # 붙여넣기
+
+            
+
+        
     
     def run(self):
         '''매크로 시작'''
@@ -303,7 +376,7 @@ class Work(QThread):
                                         self.fnMain( self.__div.get(m) , m , _j ) 
                                 print(i,'구간반복 - 종료','='*15)
                                         
-                            elif (self.__div.get(i) in ['클릭','붙여넣기','글씨쓰기','선택하기','중복선택','랜덤선택','방향전환','무시','캡쳐','D&D','랜덤대기'] ) :
+                            elif (self.__div.get(i) in ['클릭','붙여넣기','글씨쓰기','선택하기','중복선택','랜덤선택','방향전환','무시','캡쳐','D&D','랜덤대기','자방[숫자]'] ) :
                                 self.fnMain( self.__div.get(i) , i , _j )                            
 
         except Exception as e:
